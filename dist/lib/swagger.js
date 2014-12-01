eced4b251a61fd76510f7e50a31f8b77f7b800db
Thierry Lévèque <tleveque@gmail.com>
2014-12-01 16:32:55 -0500

    Fix Post bug

--- dist/lib/swagger.js
+++ dist/lib/swagger.js
@@ -1,5 +1,5 @@
 // swagger.js
-// version 2.0.41
+// version 2.0.46
 
 (function () {
 
@@ -498,7 +498,7 @@
           }
         }
         o.nickname = this.sanitize(o.nickname);
-        var op = new SwaggerOperation(o.nickname, resource_path, method, o.parameters, o.summary, o.notes, type, responseMessages, this, consumes, produces, o.authorizations);
+        var op = new SwaggerOperation(o.nickname, resource_path, method, o.parameters, o.summary, o.notes, type, responseMessages, this, consumes, produces, o.authorizations, o.deprecated);
         this.operations[op.nickname] = op;
         output.push(this.operationsArray.push(op));
       }
@@ -691,7 +691,7 @@
     return str;
   };
 
-  var SwaggerOperation = function (nickname, path, method, parameters, summary, notes, type, responseMessages, resource, consumes, produces, authorizations) {
+  var SwaggerOperation = function (nickname, path, method, parameters, summary, notes, type, responseMessages, resource, consumes, produces, authorizations, deprecated) {
     var _this = this;
 
     var errors = [];
@@ -707,6 +707,7 @@
     this.consumes = consumes;
     this.produces = produces;
     this.authorizations = authorizations;
+    this.deprecated = deprecated;
     this["do"] = __bind(this["do"], this);
 
     if (errors.length > 0) {
@@ -738,7 +739,7 @@
       }
       param.type = type;
 
-      if (type.toLowerCase() === 'boolean') {
+      if (type && type.toLowerCase() === 'boolean') {
         param.allowableValues = {};
         param.allowableValues.values = ["true", "false"];
       }
@@ -904,6 +905,12 @@
       }
       else if (param.paramType === 'form' || param.paramType.toLowerCase() === 'file')
         possibleParams.push(param);
+      else if (param.paramType === 'body' && param.name !== 'body') {
+        if (args.body) {
+          throw new Error("Saw two body params in an API listing; expecting a max of one.");
+        }
+        args.body = args[param.name];
+      }
     }
 
     if (args.body != null) {
@@ -961,7 +968,7 @@
       if (param.paramType === 'path') {
         if (args[param.name]) {
           // apply path params and remove from args
-          var reg = new RegExp('\\{\\s*?' + param.name + '.*?\\}(?=\\s*?(\\/|$))', 'gi');
+          var reg = new RegExp('\\{\\s*?' + param.name + '.*?\\}(?=\\s*?(\\/?|$))', 'gi');
           url = url.replace(reg, this.encodePathParam(args[param.name]));
           delete args[param.name];
         }
@@ -973,11 +980,21 @@
     var queryParams = "";
     for (var i = 0; i < params.length; i++) {
       var param = params[i];
-      if (param.paramType === 'query') {
-        if (args[param.name] !== undefined) {
-          if (queryParams !== '')
-            queryParams += "&";
-          queryParams += encodeURIComponent(param.name) + '=' + encodeURIComponent(args[param.name]);
+      if(param.paramType === 'query') {
+        if (queryParams !== '')
+          queryParams += '&';    
+        if (Array.isArray(param)) {
+          var j;   
+          var output = '';   
+          for(j = 0; j < param.length; j++) {    
+            if(j > 0)    
+              output += ',';   
+            output += encodeURIComponent(param[j]);    
+          }    
+          queryParams += encodeURIComponent(param.name) + '=' + output;    
+        }    
+        else {   
+          queryParams += encodeURIComponent(param.name) + '=' + encodeURIComponent(args[param.name]);                
         }
       }
     }
@@ -1476,8 +1493,8 @@
         data: response.content.data
       };
 
-      var contentType = (response._headers["content-type"] || response._headers["Content-Type"] || null)
-
+      var headers = response._headers.normalized || response._headers;
+      var contentType = (headers["content-type"] || headers["Content-Type"] || null)
       if (contentType != null) {
         if (contentType.indexOf("application/json") == 0 || contentType.indexOf("+json") > 0) {
           if (response.content.data && response.content.data !== "")
@@ -1646,6 +1663,8 @@
   var sampleModels = {};
   var cookies = {};
 
+  e.parameterMacro = parameterMacro;
+  e.modelPropertyMacro = modelPropertyMacro;
   e.SampleModels = sampleModels;
   e.SwaggerHttp = SwaggerHttp;
   e.SwaggerRequest = SwaggerRequest;
